3ba046dd0564a0d10f02e5a8d8b3571e7552e1ae
airisahani <airisahani18@gmail.com>
2024-12-13 12:51:50 +0800

    in this commit: MainActivity: - the first part of module 4 which is the NotificationAlert for the user Androidmanifest: - ensure compileSdkVersion 34 - minSdkVersion 21 // Minimum API level supported - targetSdkVersion 34 // Target API level drawable file: - i added the logo.png to be used as our Post.It logo

--- app/src/main/java/com/example/artownmad/Activities/MainActivity.java
+++ app/src/main/java/com/example/artownmad/Activities/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.artownmad.Activities;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,8 +29,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.view.View;
+import android.widget.Toast;
+import android.Manifest;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "Post.It";
+    private static final int RC_NOTIFICATION = 99;
+    private static final String PREF_NAME = "ReportStatusPrefs";
     FloatingActionButton fab;
     public BottomNavigationView bottomNavigationView;
 
@@ -32,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Request permission to allow notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, RC_NOTIFICATION);
+        }
+
+        createNotificationChannel();
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         fab = findViewById(R.id.fab);
@@ -73,7 +95,59 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-private void replaceFragment(Fragment fragment) {
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CharSequence name = "Crime";
+            String description = "Report status update";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            // Create a notification channel with the given ID, name and importance
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                channel.setDescription(description);
+
+                // Register the channel with the system
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+
+//        private void showNotification (String current){
+//            Intent intent = new Intent(this, StatusUpdateActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+//
+//            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                    .setSmallIcon(R.drawable.logo)
+//                    .setContentTitle("Report " + current)
+//                    .setContentText("The status of your report has been updated.")
+//                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                    .setAutoCancel(true)
+//                    .setContentIntent(pendingIntent);
+//
+//            NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+//                return;
+//            }
+//            manager.notify(0, builder.build());
+//        }
+
+        public void onRequestPermissionResult( int requestCode, String[] permissions,
+        int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == RC_NOTIFICATION) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                } else {
+                    // Permission denied, handle accordingly
+                    Toast.makeText(this, "Notification permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+
+    private void replaceFragment(Fragment fragment) {
     FragmentManager fragmentManager = getSupportFragmentManager();
     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
     fragmentTransaction.replace(R.id.fragment_layout, fragment);
