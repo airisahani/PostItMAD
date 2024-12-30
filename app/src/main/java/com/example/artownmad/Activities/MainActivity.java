3a0022c786e268256887574e21f2025fb7ec2733
Airina Hani <airinahani@gmail.com>
2024-12-30 10:39:54 +0800

    To do: - Need to figure out the news list for Home Fragment and make sure it works. - Beautify the alert and call page.

--- app/src/main/java/com/example/artownmad/Activities/MainActivity.java
+++ app/src/main/java/com/example/artownmad/Activities/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.artownmad.Activities;
 
-import static androidx.core.app.ActivityCompat.requestPermissions;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +23,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.artownmad.AccountFragment;
+import com.example.artownmad.Activities.HistoryActivity;
+import com.example.artownmad.CallFragment;
 import com.example.artownmad.HomeFragment;
 import com.example.artownmad.MapFragment;
 import com.example.artownmad.R;
@@ -30,6 +34,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.Manifest;
 
@@ -40,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String PREF_NAME = "ReportStatusPrefs";
     FloatingActionButton fab;
     public BottomNavigationView bottomNavigationView;
+    Button button;
+    ImageButton buttoncall;
 
 
     @Override
@@ -86,12 +94,67 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setVisibility(View.VISIBLE);
         fab.setVisibility(View.VISIBLE);
 
+        button = findViewById(R.id.BtnEmergencyNoti);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.POST_NOTIFICATIONS) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS},101);
+            }
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeNotification();
+            }
+        });
+        buttoncall = findViewById(R.id.CallButton);
+        buttoncall.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                CallFragment fragmentcall = new CallFragment();
+                transaction.replace(R.id.fragment_layout, fragmentcall);
+                transaction.commit();
+            }
+        });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void makeNotification(){
+        String chanelID = "CHANNEL_ID_NOTIFICATION";
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(getApplicationContext(), chanelID);
+        builder.setSmallIcon(R.drawable.ic_notifications_active_24)
+                .setContentTitle("Alert Notification!")
+                .setContentText("Some text for notification here")
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        Intent intent = new Intent(getApplicationContext(), AlertActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("data", "Always lock your door after leaving home!");
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),
+                0, intent, PendingIntent.FLAG_MUTABLE);
+        builder.setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = notificationManager.getNotificationChannel(chanelID);
+            if (notificationChannel == null){
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                notificationChannel = new NotificationChannel(chanelID, "Some description", importance);
+                notificationChannel.setLightColor(Color.GREEN);
+                notificationChannel.enableVibration(true);
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+        notificationManager.notify(0, builder.build());
     }
 
     private void createNotificationChannel() {
--- app/src/main/java/com/example/artownmad/SelectListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.artownmad;
-
-import com.example.artownmad.Models.NewsHeadlines;
-
-public interface SelectListener {
-    void OnNewsClicked(NewsHeadlines headlines);
-
-}
